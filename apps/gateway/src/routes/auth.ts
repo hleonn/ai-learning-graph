53a5c04b77c3076af4da06650dee0758e0145b87
Horacio Leon <edhleon2001@hotmail.com>
2026-04-07 21:56:14 -0600

    fix03: add missing Google Classroom scopes for course creation

--- apps/gateway/src/routes/auth.ts
+++ apps/gateway/src/routes/auth.ts
@@ -16,7 +16,6 @@ const oauth2Client = new google.auth.OAuth2(
 const SCOPES = [
     'https://www.googleapis.com/auth/userinfo.email',
     'https://www.googleapis.com/auth/userinfo.profile',
-    'https://www.googleapis.com/auth/classroom.courses.readonly',
     'https://www.googleapis.com/auth/classroom.courses',
     'https://www.googleapis.com/auth/classroom.coursework.me',
     'https://www.googleapis.com/auth/classroom.coursework.students',
